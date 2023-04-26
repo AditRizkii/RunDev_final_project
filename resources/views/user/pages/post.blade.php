c9dad42a192dbe7c8ee5355bb1ed7e6a2a210d26
SofiaFei <feisofiafei@gmail.com>
2023-04-27 02:09:54 +0700

    feat: add post page

---
+++ resources/views/user/pages/post.blade.php
@@ -0,0 +1,198 @@
+@extends('user.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('user.navbar.topnav', ['title' => 'Postingan'])
+
+    <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-xl-3 g-5">
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="assets/img/post/kunker.png" class="card-img-top" style="height:350px; object-fit:cover; object-position:0 68%;" alt="Kunjungan Kerja">
+                        <div class="card-body">
+                            <p class="card-text">HMIF - USK</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="assets/img/post/bukber.png" class="card-img-top" style="height:350px; object-fit:cover; object-position:0 34%;" alt="Bukber">
+                        <div class="card-body">
+                            <p class="card-text">HMIF - USK</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">2 weeks</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="assets/img/post/takjil.png" class="card-img-top" style="height:350px; object-fit:cover; object-position:0 47%;" alt="Takjil On The Road">
+                        <div class="card-body">
+                            <p class="card-text">HMIF - USK</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">1 month</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="assets/img/post/makrab.jpeg" class="card-img-top" style="height:350px; object-fit:cover; object-position:0 63%;" alt="Malam Keakraban">
+                        <div class="card-body">
+                            <p class="card-text">BEM - USK</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">1 month</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="assets/img/post/infdev.png" class="card-img-top" style="height:350px; object-fit:cover; object-position:0 40%;" alt="Informatics Developer Club">
+                        <div class="card-body">
+                            <p class="card-text">HMIF - USK</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">1 month</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="col">
+                    <div class="card shadow-sm">
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                            preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%"
+                                fill="#eceeef" dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-body-secondary">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <script src="assets/js/plugins/chartjs.min.js"></script>
+    <script>
+        var ctx1 = document.getElementById("chart-line").getContext("2d");
+
+        var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+        gradientStroke1.addColorStop(1, 'rgba(251, 99, 64, 0.2)');
+        gradientStroke1.addColorStop(0.2, 'rgba(251, 99, 64, 0.0)');
+        gradientStroke1.addColorStop(0, 'rgba(251, 99, 64, 0)');
+        new Chart(ctx1, {
+            type: "line",
+            data: {
+                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                datasets: [{
+                    label: "Mobile apps",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#fb6340",
+                    backgroundColor: gradientStroke1,
+                    borderWidth: 3,
+                    fill: true,
+                    data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                    maxBarThickness: 6
+
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                },
+                scales: {
+                    y: {
+                        grid: {
+                            drawBorder: false,
+                            display: true,
+                            drawOnChartArea: true,
+                            drawTicks: false,
+                            borderDash: [5, 5]
+                        },
+                        ticks: {
+                            display: true,
+                            padding: 10,
+                            color: '#fbfbfb',
+                            font: {
+                                size: 11,
+                                family: "Open Sans",
+                                style: 'normal',
+                                lineHeight: 2
+                            },
+                        }
+                    },
+                    x: {
+                        grid: {
+                            drawBorder: false,
+                            display: false,
+                            drawOnChartArea: false,
+                            drawTicks: false,
+                            borderDash: [5, 5]
+                        },
+                        ticks: {
+                            display: true,
+                            color: '#ccc',
+                            padding: 20,
+                            font: {
+                                size: 11,
+                                family: "Open Sans",
+                                style: 'normal',
+                                lineHeight: 2
+                            },
+                        }
+                    },
+                },
+            },
+        });
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ Route::get('/dashboard', function () {
     return view('user.pages.dashboard');
 })->middleware(['auth', 'verified', 'role:mahasiswa'])->name('dashboard');
 
+Route::get('/post', function () {
+    return view('user.pages.post');
+})->middleware(['auth', 'verified', 'role:mahasiswa'])->name('post');
+
 Route::get('/admin', function () {
     return view('admin.layouts.admin');
 })->middleware(['auth', 'verified', 'role:admin'])->name('admin.admin');
