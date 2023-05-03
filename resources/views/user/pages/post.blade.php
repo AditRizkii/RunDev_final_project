09362eb52c70f54c471e72f7be38dcff0d72a39e
SofiaFei <feisofiafei@gmail.com>
2023-05-04 03:09:17 +0700

    feat: add modal for view and adit makrab post

--- resources/views/user/pages/post.blade.php
+++ resources/views/user/pages/post.blade.php
@@ -453,14 +453,123 @@ ____________________________
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="d-flex justify-content-evenly">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary"
+                                            data-bs-toggle="modal" data-bs-target="#makrabModal">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary"
+                                            data-bs-toggle="modal" data-bs-target="#edmakrabModal">Edit</button>
                                     </div>
                                     <div class="my-2 mx-4">
                                         <i class="fa fa-duotone fa-trash text-sm"></i>
                                     </div>
                                 </div>
-                                <small class="text-body-secondary">9 mins</small>
+                                <small class="text-body-secondary">March 17</small>
+                            </div>
+
+                            <div class="modal fade" id="makrabModal" tabindex="-1" aria-labelledby="exModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                                    <div class="modal-content rounded-25">
+                                        <div class="modal-body py-0 px-0">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <img style="width: 100%; height: 100%; border-top-left-radius: 10px;
+                                                    border-bottom-left-radius: 10px;"
+                                                        src="assets/img/post/makrab.jpeg" alt="">
+                                                </div>
+                                                <div class="col mx-5 my-3 margin-left-5 text-dark">
+                                                    <div class="border-bottom border-2 px-3">
+                                                        <div class="d-flex justify-content-end">
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"><img
+                                                                    src="{{ Vite::asset('public/assets/img/x-solid.svg') }}"
+                                                                    class="w-80 h-auto" alt="close"></button>
+                                                        </div>
+                                                        <p class="fw-bold fs-3 lh-sm">BEM FMIPA (Badan Eksekutif Mahasiswa)
+                                                            - USK
+                                                        </p>
+                                                        <p class="fw-normal fs-6 px-2 lh-1 text-secondary">Dari <span
+                                                                class="text-primary">Aditya Rizki Ramadhan</span> </p>
+
+                                                    </div>
+                                                    <div class="p-3 py-4 border-bottom border-2">
+                                                        <p class="fs-5 fw-medium">Assalamualaikum,</p>
+                                                        <p class="fs-5 fw-medium">👋🏻Haloo,Selamat malam kawan kawan HMIF </p>
+                                                        <p class="fs-5 fw-medium">Keluarga Besar MIPA (KBM) bakalan ngadain Malam Keakraban (MAKRAB) pada : </p>
+                                                        <p class="fs-5 fw-medium">
+                                                            📆Hari,Tanggal : Jumat, 17 Maret 2023 <br>
+                                                            📍Tempat : Rumoh Aceh Lampineung</p>
+                                                        <p class="fs-5 fw-medium">Buat kawan kawan bisa gabung nihhh untuk join MAKRAB biar saling berkenalan dengan keluarga besar FMIPA lainnya..🤝 <br>
+                                                        Ditunggu kehadiraannyaa yaa 🫶</p>
+                                                        <p class="fs-5 fw-medium">Thankyouu👋🏻👋🏻</p>
+                                                    </div>
+                                                    <div class="p-3 flex">
+                                                        <img src="{{ Vite::asset('public/assets/img/post/likea.png') }}"
+                                                            id="like" style="width: 35px" alt=""
+                                                            onclick="changeImage(this)">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="modal fade" id="edmakrabModal" tabindex="-1" aria-labelledby="editModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                                    <div class="modal-content rounded-25">
+                                        <div class="modal-body py-0 px-0">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <img style="width: 100%; height: 100%; border-top-left-radius: 10px;
+                                                    border-bottom-left-radius: 10px;"
+                                                        src="assets/img/post/makrab.jpeg" alt="">
+                                                </div>
+                                                <div class="col mx-5 my-3 margin-left-5 text-dark">
+                                                    <div class="border-bottom border-2 px-3">
+                                                        <div class="d-flex justify-content-end">
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"><img
+                                                                    src="{{ Vite::asset('public/assets/img/x-solid.svg') }}"
+                                                                    class="w-80 h-auto" alt="close"></button>
+                                                        </div>
+                                                        <p class="fw-bold fs-3 lh-sm">BEM FMIPA (Badan Eksekutif Mahasiswa)
+                                                            - USK
+                                                        </p>
+                                                        <p class="fw-normal fs-6 px-2 lh-1 text-secondary">Dari <span
+                                                                class="text-primary">Aditya Rizki Ramadhan</span> </p>
+
+                                                    </div>
+                                                    <div class="p-3 py-4">
+                                                        <div class="md-form flex">
+                                                            <label for="form10" class="fs-6 fw-base mx-1">Edit
+                                                                Caption</label>
+                                                            <i class="fas fa-pencil-alt prefix"></i>
+                                                            <textarea id="form10" class="xl-textarea form-control mb-3" rows="15">Assalamualaikum,
+👋🏻Haloo,Selamat malam kawan kawan HMIF 
+                                                                
+Keluarga Besar MIPA (KBM) bakalan ngadain Malam Keakraban (MAKRAB) pada :
+
+📆Hari,Tanggal : Jumat, 17 Maret 2023
+📍Tempat : Rumoh Aceh Lampineung
+                                                                
+Buat kawan kawan bisa gabung nihhh untuk join MAKRAB biar saling berkenalan dengan keluarga besar FMIPA lainnya..🤝
+Ditunggu kehadiraannyaa yaa 🫶
+                                                                
+Thankyouu👋🏻👋🏻
+                                                                </textarea>
+                                                            <div class="d-flex justify-content-end mx-5">
+                                                                <button type="submit"
+                                                                    class="btn btn-primary my-4 ">Simpan</button>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
