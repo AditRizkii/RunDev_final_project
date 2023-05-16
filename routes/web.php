417ec85b5c1456dd731a69e862fd7894dfffb7c0
tyararayna <tyara.raynasari@gmail.com>
2023-05-16 22:12:15 +0700

    fix: change route

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::middleware(['auth', 'verified', 'role:admin'])->name('admin.')->prefix('a
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/{id_alamat}', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::post('/kota', [AlamatController::class, 'getKota'])->name('kota');
